2ee9d23f3a1782b9ff308c4661a0284d55df6cef
Mikael Myyrä <mikael.myyrae@gmail.com>
2022-12-28 17:24:15 +0200

    Abstract running and visualizing simulations into reusable class

--- PydecProtos/leapfrog_dirichlet.py
+++ PydecProtos/leapfrog_dirichlet.py
@@ -12,13 +12,12 @@ The boundary condition u = 0 implies v = 0, but w can vary on the boundary.
 """
 
 import mesh
+from sim_runner import Simulation
 
-import functools
-import matplotlib.animation
-import matplotlib.pyplot as plt
 import numpy as np
 
-# mesh setup
+# shared mesh for all simulations in this file
+# (TODO: load mesh from file to avoid work regenerating every time)
 
 mesh_dim = np.pi
 verts_per_side = 20
@@ -26,79 +25,65 @@ mesh_scale = mesh_dim / verts_per_side
 cmp_mesh = mesh.rect_unstructured(mesh_dim, mesh_dim, mesh_scale)
 cmp_complex = cmp_mesh.as_pydec_complex()
 
-# TODO: initial condition should maybe be specified on u
-# and then computed from that for v and w? look at papers, how do they do this?
 
-# initial condition v = sin(nx)sin(my), w = 0
-x_wave_count = 2
-y_wave_count = 3
-v = np.sin(x_wave_count * cmp_mesh.vertices[:, 0]) * np.sin(
-    y_wave_count * cmp_mesh.vertices[:, 1]
-)
-w = np.zeros(cmp_complex[1].num_simplices)
+class StandingWave(Simulation):
+    def __init__(self):
+        # time stepping
 
-# time stepping
+        steps_per_unit_time = 20
+        dt = 1.0 / float(steps_per_unit_time)
+        sim_time_units = 6
+        step_count = steps_per_unit_time * sim_time_units
 
-steps_per_unit_time = 20
-dt = 1.0 / float(steps_per_unit_time)
-sim_time_units = 6
-step_count = steps_per_unit_time * sim_time_units
+        # multiplier matrix for w in the time-stepping formula for v
+        v_step_mat = (
+            -dt * cmp_complex[0].star_inv * cmp_complex[0].d.T * cmp_complex[1].star
+        )
+        # enforce boundary condition by setting rows of this matrix to 0 for boundary vertices
+        for bound_edge in cmp_complex.boundary():
+            for bound_vert in bound_edge.boundary():
+                vert_idx = cmp_complex[0].simplex_to_index.get(bound_vert)
+                v_step_mat[vert_idx, :] = 0.0
+        # multiplier matrix for v in the time-stepping formula for w
+        w_step_mat = dt * cmp_complex[0].d
 
-# TODO: add explicit wavenumber, currently it's implicitly 1
+        self.v_step_mat = v_step_mat
+        self.w_step_mat = w_step_mat
+        super().__init__(mesh=cmp_mesh, dt=dt, step_count=step_count, zlim=[-1.5, 1.5])
 
-# multiplier matrix for w in the time-stepping formula for v
-v_step_mat = -dt * cmp_complex[0].star_inv * cmp_complex[0].d.T * cmp_complex[1].star
-# enforce boundary condition by setting rows of this matrix to 0 for boundary vertices
-edge_vert_indices = []  # store edge vertex indices for debugging
-for bound_edge in cmp_complex.boundary():
-    for bound_vert in bound_edge.boundary():
-        vert_idx = cmp_complex[0].simplex_to_index.get(bound_vert)
-        v_step_mat[vert_idx, :] = 0.0
-        edge_vert_indices.append(vert_idx)
-edge_vert_indices = set(edge_vert_indices)
-# multiplier matrix for v in the time-stepping formula for w
-w_step_mat = dt * cmp_complex[0].d
+    def init_state(self):
+        # TODO: initial condition should maybe be specified on u
+        # and then computed from that for v and w? look at papers, how do they do this?
+        x_wave_count = 2
+        y_wave_count = 3
+        self.v = np.sin(x_wave_count * self.mesh.vertices[:, 0]) * np.sin(
+            y_wave_count * self.mesh.vertices[:, 1]
+        )
+        # TODO: w should be vector-valued, is this working as intended?
+        self.w = np.zeros(cmp_complex[1].num_simplices)
 
-# setup animated drawing
+    def step(self):
+        self.v += self.v_step_mat * self.w
+        self.w += self.w_step_mat * self.v
 
-fig = plt.figure(layout="tight")
-ax = fig.add_subplot(1, 1, 1, projection="3d")
-
-
-# setup drawing and time stepping
-
-
-def draw_mesh(z_data):
-    ax.clear()
-    ax.set_zlim([-1, 1])
-    return ax.plot_trisurf(
-        cmp_mesh.vertices[:, 0],
-        cmp_mesh.vertices[:, 1],
-        z_data,
-        cmap="viridis",
-        edgecolor="none",
-    )
+    def get_z_data(self):
+        # TODO: what if I want to visualize the original function `u`
+        # from the second-order equation?
+        return self.v
 
 
-def step_and_draw(step_idx, v, w):
-    # TODO: add source term to v
-    v += v_step_mat * w
-    w += w_step_mat * v
+# another simulation with the same equations but different initial conditions
+class MovingWave(StandingWave):
+    def init_state(self):
+        x = self.mesh.vertices[:, 0]
+        y = self.mesh.vertices[:, 1]
+        self.v = np.multiply(0.2 * np.multiply(x, x), np.sin(3 * x)) * np.sin(y)
+        self.w = np.zeros(cmp_complex[1].num_simplices)
 
-    return draw_mesh(v)
 
+sim1 = StandingWave()
+sim1.show()
 
-anim = matplotlib.animation.FuncAnimation(
-    fig=fig,
-    func=functools.partial(step_and_draw, v=v, w=w),
-    frames=step_count,
-    interval=int(1000 * dt),
-)
-
-# TODO: CLI argument for this
-save_gif = False
-if save_gif:
-    print("Saving gif. This takes a while")
-    anim.save("result.gif", writer="imagemagick", fps=steps_per_unit_time)
-
-plt.show()
+sim2 = MovingWave()
+sim2.show()
+# sim2.save_mp4()
---
+++ PydecProtos/sim_runner.py
@@ -0,0 +1,89 @@
+"""Abstractions for quickly making time-dependent DEC simulations animated using matplotlib."""
+
+import matplotlib.animation as plt_anim
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as plt3d
+import numpy as np
+import numpy.typing as npt
+from abc import ABC
+from abc import abstractmethod
+from dataclasses import dataclass
+
+from mesh import Mesh
+
+
+@dataclass(init=False)
+class Simulation(ABC):
+    # parameters
+    mesh: Mesh
+    dt: float
+    step_count: int
+    zlim: list[float]
+    # internals
+    fig: plt.Figure
+    ax: plt3d.Axes3D
+    anim: plt_anim.FuncAnimation
+
+    def __init__(self, mesh: Mesh, dt: float, step_count: int, zlim: list[float]):
+        self.mesh = mesh
+        self.dt = dt
+        self.step_count = step_count
+        self.zlim = zlim
+
+        self.fig = plt.figure(layout="tight")
+        self.ax = self.fig.add_subplot(1, 1, 1, projection="3d")
+        self.anim = plt_anim.FuncAnimation(
+            fig=self.fig,
+            init_func=lambda: self._draw(),
+            func=lambda _: self._step_and_draw(),
+            frames=step_count,
+            interval=int(1000 * dt),
+        )
+
+    @abstractmethod
+    def init_state(self):
+        """Set up the initial conditions for the simulation."""
+        pass
+
+    @abstractmethod
+    def step(self):
+        """Run one timestep forward in time."""
+        pass
+
+    @abstractmethod
+    def get_z_data(self) -> npt.NDArray[np.float64]:
+        """Get the vector containing the z axis data to draw."""
+        pass
+
+    def _draw(self):
+        self.ax.clear()
+        self.ax.set_zlim3d(self.zlim)
+        return self.ax.plot_trisurf(
+            self.mesh.vertices[:, 0],
+            self.mesh.vertices[:, 1],
+            self.get_z_data(),
+            cmap="viridis",
+            edgecolor="none",
+        )
+
+    def _step_and_draw(self):
+        self.step()
+        return self._draw()
+
+    def show(self):
+        """Run the simulation from the beginning and show it in an interactive window."""
+        self.init_state()
+        plt.show()
+
+    def save_gif(self, filename: str = "result.gif"):
+        """Run the simulation from the beginning and save it to a .gif file."""
+        print(f"Saving {filename}. This takes a while")
+        self.init_state()
+        writer = plt_anim.ImageMagickWriter(fps=int(1.0 / self.dt))
+        self.anim.save(filename, writer)
+
+    def save_mp4(self, filename: str = "result.mp4"):
+        print(f"Saving {filename}. This takes a while")
+        self.init_state()
+        writer = plt_anim.FFMpegWriter(fps=int(1.0 / self.dt))
+        self.anim.save(filename, writer)
