615bf1f402cf65ff0ad8fdd3d31023198beca659
Mikael Myyrä <mikael.myyrae@gmail.com>
2023-01-10 00:38:29 +0200

    circle scatterer: Implement scatterer boundary condition

--- PydecProtos/circle_scatterer.py
+++ PydecProtos/circle_scatterer.py
@@ -11,6 +11,7 @@ import mesh
 from sim_runner import Simulation
 
 import numpy as np
+import numpy.typing as npt
 import math
 import matplotlib.pyplot as plt
 import pydec
@@ -56,7 +57,7 @@ class CircleScatterer(Simulation):
         inc_wavenumber = 1.0
         inc_wave_dir = np.array([0.0, 1.0])
         self.inc_wave_vector = inc_wavenumber * inc_wave_dir
-        self.inc_angular_vel = 1.0
+        self.inc_angular_vel = 3.0
 
         # time parameters
         sim_time = 2.0 * np.pi
@@ -69,7 +70,7 @@ class CircleScatterer(Simulation):
         )
         self.w_step_mat = dt * cmp_complex[0].d
 
-        super().__init__(mesh=cmp_mesh, dt=dt, step_count=step_count, zlim=[-3.0, 3.0])
+        super().__init__(mesh=cmp_mesh, dt=dt, step_count=step_count, zlim=[-8.0, 8.0])
 
     def init_state(self):
         # time needed for incoming wave evaluation
@@ -77,11 +78,14 @@ class CircleScatterer(Simulation):
 
         self.v = np.zeros(cmp_complex[0].num_simplices)
         for vert_idx in range(len(self.v)):
+            # time derivative of the incoming wave
             self.v[vert_idx] = -self.inc_angular_vel * math.sin(
                 np.dot(self.inc_wave_vector, self.mesh.vertices[vert_idx, :])
             )
-        # TODO: setting values for this requires integration over edges, figure it out
         self.w = np.zeros(cmp_complex[1].num_simplices)
+        for edge_idx in range(len(self.w)):
+            edge = cmp_complex[1].simplices[edge_idx]
+            self.w[edge_idx] = self._eval_inc_wave_w(0.5 * self.dt, edge)
 
     def step(self):
         self.t += self.dt
@@ -93,13 +97,32 @@ class CircleScatterer(Simulation):
                 self.inc_angular_vel * self.t
                 - np.dot(self.inc_wave_vector, cmp_mesh.vertices[vert_idx])
             )
+        # w is computed at a time instance offset by half dt
+        t_at_w = self.t + 0.5 * self.dt
         self.w += self.w_step_mat * self.v
-        # TODO: inner boundary condition for w and outer boundary condition for both variables
+        # inner edge boundary condition for w
+        for bound_edge in inner_bound_edges:
+            edge_idx = cmp_complex[1].simplex_to_index.get(bound_edge)
+            self.w[edge_idx] = self._eval_inc_wave_w(t_at_w, bound_edge.boundary())
+
+        # TODO: outer absorbing boundary condition
 
     def get_z_data(self):
         # visualizing acoustic pressure
         return self.v
 
+    def _eval_inc_wave_w(self, t: float, edge_verts: list[npt.NDArray]) -> float:
+        """Evaluate the line integral of the particle velocity of the incoming wave
+        over an edge of the mesh, in other words compute a value of `w` from the wave."""
+
+        p1 = self.mesh.vertices[edge_verts[0]]
+        p2 = self.mesh.vertices[edge_verts[1]]
+        kdotp = np.dot(self.inc_wave_vector, p1)
+        kdotl = np.dot(self.inc_wave_vector, p2 - p1)
+        angle = self.inc_angular_vel * t
+        return math.cos(angle - kdotp) - math.cos(angle - kdotp - kdotl)
+
 
 sim = CircleScatterer()
 sim.show()
+# sim.save_gif()
