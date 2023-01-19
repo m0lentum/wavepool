e06480cc139cbb24aa642f60eef3c561bb9b7b68
Mikael Myyrä <mikael.myyrae@gmail.com>
2023-01-20 00:07:02 +0200

    Add error measurement to accuracy test prototype

--- PydecProtos/accuracy_test.py
+++ PydecProtos/accuracy_test.py
@@ -2,7 +2,8 @@
 Wave equation (see membrane.py doc comment).
 Square domain with known plane wave solution
 imposed on the edge as Dirichlet boundary condition.
-Examining error compared to known solution.
+Examining error compared to known solution as the
+mesh is refined and timestep shortened.
 """
 
 import mesh
@@ -14,43 +15,45 @@ import math
 import matplotlib.pyplot as plt
 import pydec
 
-# mesh parameters
-mesh_dim = np.pi
-verts_per_side = 20
-mesh_scale = mesh_dim / verts_per_side
-cmp_mesh = mesh.rect_unstructured(mesh_dim, mesh_dim, mesh_scale)
-cmp_complex = pydec.SimplicialComplex(cmp_mesh)
-
-# time parameters
-sim_time = 2.0 * np.pi
-dt = 1.0 / 20.0
-step_count = math.ceil(sim_time / dt)
-
-# incoming wave parameters
-inc_wavenumber = 2.0
-inc_wave_dir = np.array([0.0, 1.0])
-inc_wave_vector = inc_wavenumber * inc_wave_dir
-inc_angular_vel = 2.0
-
-# gather boundary elements
-bound_verts = []
-bound_edges = []
-for edge in cmp_complex.boundary():
-    edge_verts = edge.boundary()
-    bound_edges.append(edge)
-    bound_verts.extend(edge_verts)
-# remove duplicates by constructing sets
-bound_verts = set(bound_verts)
-bound_edges = set(bound_edges)
-
 
 class AccuracyTest(Simulation):
-    def __init__(self):
+    def __init__(self, mesh_subdivisions: int, timesteps_per_second: int):
+        # mesh parameters
+        mesh_dim = np.pi
+        mesh_scale = mesh_dim / mesh_subdivisions
+        cmp_mesh = mesh.rect_unstructured(mesh_dim, mesh_dim, mesh_scale)
+        self.cmp_complex = pydec.SimplicialComplex(cmp_mesh)
+
+        # time parameters
+        sim_time = 2.0 * np.pi
+        dt = 1.0 / timesteps_per_second
+        step_count = math.ceil(sim_time / dt)
+
+        # incoming wave parameters
+        inc_wavenumber = 2.0
+        inc_wave_dir = np.array([0.0, 1.0])
+        self.inc_wave_vector = inc_wavenumber * inc_wave_dir
+        self.inc_angular_vel = 2.0
+
+        # gather boundary elements
+        bound_verts = []
+        bound_edges = []
+        for edge in self.cmp_complex.boundary():
+            edge_verts = edge.boundary()
+            bound_edges.append(edge)
+            bound_verts.extend(edge_verts)
+        # remove duplicates by constructing sets
+        self.bound_verts = set(bound_verts)
+        self.bound_edges = set(bound_edges)
+
         # time stepping matrices
         self.v_step_mat = (
-            -dt * cmp_complex[0].star_inv * cmp_complex[0].d.T * cmp_complex[1].star
+            -dt
+            * self.cmp_complex[0].star_inv
+            * self.cmp_complex[0].d.T
+            * self.cmp_complex[1].star
         )
-        self.w_step_mat = dt * cmp_complex[0].d
+        self.w_step_mat = dt * self.cmp_complex[0].d
 
         super().__init__(mesh=cmp_mesh, dt=dt, step_count=step_count, zlim=[-8.0, 8.0])
 
@@ -58,51 +61,103 @@ class AccuracyTest(Simulation):
         # time needed for incoming wave evaluation
         self.t = 0.0
 
-        self.v = np.zeros(cmp_complex[0].num_simplices)
+        self.v = np.zeros(self.cmp_complex[0].num_simplices)
         for vert_idx in range(len(self.v)):
             # time derivative of the incoming wave
-            self.v[vert_idx] = inc_angular_vel * math.sin(
-                -np.dot(inc_wave_vector, self.mesh.vertices[vert_idx, :])
+            self.v[vert_idx] = self._eval_inc_wave_pressure(
+                0.0, self.mesh.vertices[vert_idx]
             )
-        self.w = np.zeros(cmp_complex[1].num_simplices)
+        self.w = np.zeros(self.cmp_complex[1].num_simplices)
         for edge_idx in range(len(self.w)):
-            edge = cmp_complex[1].simplices[edge_idx]
-            self.w[edge_idx] = self._eval_inc_wave_w(0.5 * self.dt, edge)
+            edge = self.cmp_complex[1].simplices[edge_idx]
+            self.w[edge_idx] = self._eval_inc_wave_velocity(0.5 * self.dt, edge)
 
     def step(self):
         self.t += self.dt
         self.v += self.v_step_mat * self.w
         # plane wave at the boundary for v
-        for bound_vert in bound_verts:
-            vert_idx = cmp_complex[0].simplex_to_index.get(bound_vert)
-            self.v[vert_idx] = inc_angular_vel * math.sin(
-                inc_angular_vel * self.t
-                - np.dot(inc_wave_vector, cmp_mesh.vertices[vert_idx])
+        for bound_vert in self.bound_verts:
+            vert_idx = self.cmp_complex[0].simplex_to_index.get(bound_vert)
+            self.v[vert_idx] = self._eval_inc_wave_pressure(
+                self.t, self.mesh.vertices[vert_idx]
             )
         # w is computed at a time instance offset by half dt
         t_at_w = self.t + 0.5 * self.dt
         self.w += self.w_step_mat * self.v
         # plane wave at the boundary for w
-        for bound_edge in bound_edges:
-            edge_idx = cmp_complex[1].simplex_to_index.get(bound_edge)
-            self.w[edge_idx] = self._eval_inc_wave_w(t_at_w, bound_edge.boundary())
+        for bound_edge in self.bound_edges:
+            edge_idx = self.cmp_complex[1].simplex_to_index.get(bound_edge)
+            self.w[edge_idx] = self._eval_inc_wave_velocity(
+                t_at_w, bound_edge.boundary()
+            )
 
     def get_z_data(self):
         # visualizing acoustic pressure
         return self.v
 
-    def _eval_inc_wave_w(self, t: float, edge_verts: list[npt.NDArray]) -> float:
+    def _eval_inc_wave_pressure(self, t, position: npt.NDArray) -> float:
+        """Evaluate the value of v for the incoming plane wave at a point."""
+
+        return self.inc_angular_vel * math.sin(
+            self.inc_angular_vel * t - np.dot(self.inc_wave_vector, position)
+        )
+
+    def _eval_inc_wave_velocity(self, t: float, edge_verts: list[npt.NDArray]) -> float:
         """Evaluate the line integral of the particle velocity of the incoming wave
         over an edge of the mesh, in other words compute a value of `w` from the wave."""
 
         p1 = self.mesh.vertices[edge_verts[0]]
         p2 = self.mesh.vertices[edge_verts[1]]
-        kdotp = np.dot(inc_wave_vector, p1)
-        kdotl = np.dot(inc_wave_vector, p2 - p1)
-        angle = inc_angular_vel * t
+        kdotp = np.dot(self.inc_wave_vector, p1)
+        kdotl = np.dot(self.inc_wave_vector, p2 - p1)
+        angle = self.inc_angular_vel * t
         return math.cos(angle - kdotp) - math.cos(angle - kdotp - kdotl)
 
+    def current_max_pressure_error(self) -> float:
+        """Find the largest deviation from the analytical plane wave solution's pressure
+        in the current state of the simulation."""
 
-sim = AccuracyTest()
-sim.show()
-# sim.save_mp4()
+        max_err = 0.0
+        for vert, v_val in zip(self.mesh.vertices, self.v):
+            exact_pressure = self._eval_inc_wave_pressure(self.t, vert)
+            err = abs(exact_pressure - v_val)
+            if err > max_err:
+                max_err = err
+        return max_err
+
+    def current_max_velocity_error(self) -> float:
+        """Find the largest deviation from the analytical plane wave solution's velocity
+        in the current state of the simulation."""
+
+        max_err = 0.0
+        for edge_idx in range(len(self.w)):
+            w_val = self.w[edge_idx]
+            edge = self.cmp_complex[1].simplices[edge_idx]
+            exact_vel = self._eval_inc_wave_velocity(self.t + 0.5 * self.dt, edge)
+            err = abs(exact_vel - w_val)
+            if err > max_err:
+                max_err = err
+        return max_err
+
+
+div_counts = [5, 10, 20, 40, 60]
+sims = [AccuracyTest(mesh_subdivisions=n, timesteps_per_second=40) for n in div_counts]
+sims[0].show()
+sims[-1].show()
+
+v_errors = []
+w_errors = []
+for sim, div_count in zip(sims, div_counts):
+    sim.run_to_end()
+    v_errors.append(sim.current_max_pressure_error())
+    w_errors.append(sim.current_max_velocity_error())
+
+fig = plt.figure()
+v_ax = fig.add_subplot(2, 1, 1)
+v_ax.set(xlabel="mesh division count", ylabel="max error in pressure")
+v_ax.plot(div_counts, v_errors)
+w_ax = fig.add_subplot(2, 1, 2)
+w_ax.set(xlabel="mesh division count", ylabel="max error in velocity")
+w_ax.plot(div_counts, w_errors)
+plt.show()
+fig.savefig("errors.png")
--- PydecProtos/sim_runner.py
+++ PydecProtos/sim_runner.py
@@ -9,6 +9,16 @@ import pydec
 from abc import ABC
 from abc import abstractmethod
 from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class _Animation:
+    """Matplotlib animation for visualizing simulations."""
+
+    fig: plt.Figure
+    ax: plt3d.Axes3D
+    anim: plt_anim.FuncAnimation
 
 
 @dataclass(init=False)
@@ -18,28 +28,37 @@ class Simulation(ABC):
     dt: float
     step_count: int
     zlim: list[float]
-    # internals
-    fig: plt.Figure
-    ax: plt3d.Axes3D
-    anim: plt_anim.FuncAnimation
+
+    # animation is created lazily when drawing is wanted -
+    # we may also just want to run a simulation without animating it
+    # in case we're measuring it or doing specialized drawing
+    anim: Optional[_Animation] = None
 
     def __init__(
-        self, mesh: pydec.SimplicialMesh, dt: float, step_count: int, zlim: list[float]
+        self,
+        mesh: pydec.SimplicialMesh,
+        dt: float,
+        step_count: int,
+        zlim: list[float],
     ):
         self.mesh = mesh
         self.dt = dt
         self.step_count = step_count
         self.zlim = zlim
 
-        self.fig = plt.figure(layout="tight")
-        self.ax = self.fig.add_subplot(1, 1, 1, projection="3d")
-        self.anim = plt_anim.FuncAnimation(
-            fig=self.fig,
+    def _init_animation(self):
+        """Create a matplotlib animation lazily only when drawing functions are called."""
+
+        fig = plt.figure(layout="tight")
+        ax = fig.add_subplot(1, 1, 1, projection="3d")
+        anim = plt_anim.FuncAnimation(
+            fig=fig,
             init_func=lambda: self._draw(),
             func=lambda _: self._step_and_draw(),
-            frames=step_count,
-            interval=int(1000 * dt),
+            frames=self.step_count,
+            interval=int(1000 * self.dt),
         )
+        self.anim = _Animation(fig, ax, anim)
 
     @abstractmethod
     def init_state(self):
@@ -58,9 +77,12 @@ class Simulation(ABC):
         pass
 
     def _draw(self):
-        self.ax.clear()
-        self.ax.set_zlim3d(self.zlim)
-        return self.ax.plot_trisurf(
+        if self.anim is None:
+            raise Exception("Animation wasn't created before drawing")
+
+        self.anim.ax.clear()
+        self.anim.ax.set_zlim3d(self.zlim)
+        return self.anim.ax.plot_trisurf(
             self.mesh.vertices[:, 0],
             self.mesh.vertices[:, 1],
             self.get_z_data(),
@@ -75,18 +97,34 @@ class Simulation(ABC):
 
     def show(self):
         """Run the simulation from the beginning and show it in an interactive window."""
+
+        self._init_animation()
         self.init_state()
         plt.show()
 
+    def run_to_end(self):
+        """Run the simulation until the specified end time without animating it."""
+
+        self.init_state()
+        for _ in range(self.step_count):
+            self.step()
+
     def save_gif(self, filename: str = "result.gif"):
         """Run the simulation from the beginning and save it to a .gif file."""
+
         print(f"Saving {filename}. This takes a while")
+        self._init_animation()
+        assert self.anim is not None
         self.init_state()
         writer = plt_anim.ImageMagickWriter(fps=int(1.0 / self.dt))
-        self.anim.save(filename, writer)
+        self.anim.anim.save(filename, writer)
 
     def save_mp4(self, filename: str = "result.mp4"):
+        """Run the simulation from the beginning and save it to a .mp4 file."""
+
         print(f"Saving {filename}. This takes a while")
+        self._init_animation()
+        assert self.anim is not None
         self.init_state()
         writer = plt_anim.FFMpegWriter(fps=int(1.0 / self.dt))
-        self.anim.save(filename, writer)
+        self.anim.anim.save(filename, writer)
